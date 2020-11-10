b2b1dbb58ca6d1b3097c0e56293edc7acae47448
JCClark1018 <JCClark1018@gmail.com>
2020-11-09 18:07:19 -0600

    completed flask server/app.py

--- app.py
+++ app.py
@@ -35,15 +35,16 @@ def welcome():
     """List all available api routes."""
     return (
         f"Available Routes:<br/>"
-        f"/api/v1.0/precip<br/>"
+        f"/api/v1.0/precipitation<br/>"
         f"/api/v1.0/stations<br/>"
         f"/api/v1.0/tobs<br/>"
-        f"/api/v1.0/start_date<br/>"
-        f"/api/v1.0/start_date/end_date<br/><br/>"
+        f"/api/v1.0/startDate<br/>"
+        f"/api/v1.0/startDate/endDate<br/><br/>"
+        f"Input dates as: YYYY-MM-DD"
     )
 
 @app.route("/api/v1.0/precip")
-def precip():
+def precipitation():
     # Create our session (link) from Python to the DB
     session = Session(engine)
 
@@ -52,11 +53,90 @@ def precip():
 
     session.close()
     
-    for date, precip in results:
-        datadict = {}
-        datadict['date'] = date
-        datadict['precip'] = precip
-        precipdict.append(datadict)
+    precipL = []
+    for date, prcp in results:
+        precip_dict = {}
+        precip_dict['date'] = date
+        precip_dict['precip'] = prcp
+        precipL.append(precip_dict)
     
-    return jsonify(precipdict)
+    # convert list
+    
+    return jsonify(precipL)
 
+@app.route("/api/v1.0/stations")
+def stations():
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
+
+    # query all station names
+    results = session.query(station.name).all()
+    session.close()
+
+    #conversions
+    AStations = list(np.ravel(results))
+    return jsonify(AStations)
+
+@app.route("/api/v1.0/tobs")
+def tobs():
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
+
+    # query dates and temperature observations for the most active station 
+    results = session.query(measurement.date, measurement.tobs).\
+                filter(measurement.station == 'USC00519281', measurement.date > "2016-08-23").all()
+
+    # convert the results into a dictionary of temp observed by date 
+    tobsL = []
+
+    for date, temp in results:
+        tobs_dict = {}
+        tobs_dict['date'] = date
+        tobs_dict['temp'] = temp
+        tobsL.append(tobs_dict)
+
+    #convert
+    return jsonify(tobsL)
+
+
+@app.route("/api/v1.0/<start>")
+def start(start):
+
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
+    
+    #select and filter
+    sel = [func.min(measurement.tobs), 
+    func.max(measurement.tobs), 
+    func.avg(measurement.tobs),]
+    calc = session.query(*sel).filter(measurement.date >= start).all()
+    calc
+
+    session.close()
+
+    # Convert
+    astart = list(np.ravel(calc))
+    return jsonify(astart)
+
+@app.route("/api/v1.0/<start>/<end>")
+def start_end(start, end):
+
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
+
+    sel = [func.min(measurement.tobs), 
+    func.max(measurement.tobs), 
+    func.avg(measurement.tobs),]
+    calc2= session.query(*sel).\
+        filter(measurement.date >= start).\
+        filter(measurement.date <= end).all()
+    calc2
+
+    session.close()
+
+    # Convert 
+    astart_end = list(np.ravel(calc2))
+    return jsonify (astart_end)
+
+if __name__ == '__main__':
+    app.run(debug=True)  
